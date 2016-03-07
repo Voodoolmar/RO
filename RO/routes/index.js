2b27be392cf577a39cfba9bcdbf916d781ff61c9
Poll <poll-1@mail.ru>
2016-03-07 17:18:56 +0600

    1

--- RO/routes/index.js
+++ RO/routes/index.js
@@ -4,8 +4,14 @@
  */
 
 exports.index = function (req, res) {
+	var files = [];
+    walkSync('public/img/portfolio', function (filePath, stat) {
+	    files.push(filePath);
+    });
+
     res.render('index', {
-        title: 'Ремонт и отделка'
+        title: 'Ремонт и отделка',
+        files: files
     });
 };
 
@@ -15,3 +21,17 @@ exports.price = function (req, res) {
         price: price.price
     });
 };
+
+function walkSync(currentDirPath, callback) {
+    var fs = require('fs'),
+        path = require('path');
+    fs.readdirSync(currentDirPath).forEach(function (name) {
+        var filePath = path.join(currentDirPath, name);
+        var stat = fs.statSync(filePath);
+        if (stat.isFile()) {
+            callback(path.basename(filePath), stat);
+        } else if (stat.isDirectory()) {
+            walkSync(path.basename(filePath), callback);
+        }
+    });
+}
